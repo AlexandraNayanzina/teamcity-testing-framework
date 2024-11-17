5f3a16b051153229568616add2f2349399f67f64
AlexandraNayanzina <alexandranayanzina@gmail.com>
2024-11-16 22:02:49 -0500

    Main workflow for the CI pipeline

---
+++ src/test/java/org/example/teamcity/ui/SetupServerTest.java
@@ -0,0 +1,14 @@
+package org.example.teamcity.ui;
+
+import com.example.teamcity.ui.pages.setup.FirstStartPage;
+import org.testng.annotations.Test;
+
+public class SetupServerTest extends BaseUiTest{
+
+    @Test(groups = {"Setup"})
+    public void setupTeamCityServerTest() {
+        FirstStartPage
+                .open()
+                .setupFirstStart();
+    }
+}
